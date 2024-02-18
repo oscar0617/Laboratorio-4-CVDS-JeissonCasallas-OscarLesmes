53295bb57cc0bbd5fff6d7fb48da934791bf48b9
oscar0617 <oscarsantiagolesmes@gmail.com>
2024-02-18 13:39:10 -0500

    modificaciones

--- src/main/java/hangman/setup/guice/HangmanFactoryServices.java
+++ src/main/java/hangman/setup/guice/HangmanFactoryServices.java
@@ -14,7 +14,10 @@ public class HangmanFactoryServices extends com.google.inject.AbstractModule {
     @Override
     protected void configure() {
         /* Guice dependency injection */
-        // bind(Interface.class).to(Concrete.class);
+        bind(GameScore.class).to(OriginalScore.class);
+        bind(Language.class).to(English.class);
+        bind(HangmanDictionary.class).to(EnglishDictionaryDataSource.class);
+        bind(HangmanPanel.class).to(HangmanStickmanPanel.class);
     }
 
 }
